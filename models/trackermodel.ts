95b295ec49e4b91fb6c20976c6061811a882ad62
Shourya <shourya0864@gmail.com>
2025-03-30 15:11:23 +0530

    shourya changes 4 30/3/2025

---
+++ models/trackermodel.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const trackerSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  signup: {
+    type: Boolean,
+    default: false,
+    required: true
+  },
+  formFilling: {
+    type: Boolean,
+    default: false,
+    required: true
+  },
+  formSubmission: {
+    type: Boolean,
+    default: false,
+    required: true
+  },
+  submissionResult: {
+    type: Boolean,
+    default: false,
+    required: true
+  }
+});
+
+const Tracker = mongoose.models.Tracker || mongoose.model('Tracker', trackerSchema);
+
+export default Tracker;
